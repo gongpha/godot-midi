d9df934d8c1e8d84b379039ef35fae6d201cfa88
Kongfa Waroros <gongpha@hotmail.com>
2026-03-04 13:32:20 +0700

    change macros to identifiers (for SCU build)

--- register_types.cpp
+++ register_types.cpp
@@ -6,15 +6,16 @@
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 using namespace godot;
+#else
+#include "modules/register_module_types.h"
 #endif
 
 #include "src/library.h"
 
-void GDXMOD_LIBRARY_INITIALIZE_NAME {
+void initialize_midi_module(ModuleInitializationLevel p_level) {
 	initialize_library_midi(p_level);
 }
-
-void GDXMOD_LIBRARY_UNINITIALIZE_NAME {
+void uninitialize_midi_module(ModuleInitializationLevel p_level) {
 	uninitialize_library_midi(p_level);
 }
 
@@ -24,8 +25,8 @@ extern "C"
 	GDExtensionBool GDE_EXPORT library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
 	{
 		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-		init_obj.register_initializer(GDXMOD_LIBRARY_INITIALIZE_FUNC);
-		init_obj.register_terminator(GDXMOD_LIBRARY_UNINITIALIZE_FUNC);
+		init_obj.register_initializer(initialize_midi_module);
+		init_obj.register_terminator(uninitialize_midi_module);
 		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 		return init_obj.init();
